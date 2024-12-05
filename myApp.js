37879071335e625f4b50c6bc5d73c65ae20cdf2e
macbook <ogbochinedu95@gmail.com>
2024-12-05 16:50:45 +0100

    updates

--- myApp.js
+++ myApp.js
@@ -23,4 +23,11 @@ app.get('/json', (req, res) => {
   res.json({ message });
 });
 
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ time: req.time });
+});
+
 module.exports = app;
