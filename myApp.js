c350f543fc96b826a2b450f93f20bee355e699fb
macbook <ogbochinedu95@gmail.com>
2024-12-05 15:23:27 +0100

    Hello World

--- myApp.js
+++ myApp.js
@@ -2,7 +2,8 @@ let express = require('express');
 let app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    console.log("Hello World")
+  res.send('Hello World');
 });
 
 module.exports = app;
