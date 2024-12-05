38e9cf6ef6f81e54f9441b0b09735de19a55d6fd
macbook <ogbochinedu95@gmail.com>
2024-12-05 15:35:13 +0100

    form

--- myApp.js
+++ myApp.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 let path = require('path');
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
